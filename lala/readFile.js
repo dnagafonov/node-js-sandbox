6cae87b7a191f0ef8a0ab53614d1bf5b9070fcb9
dnagafonov <denisagafonovwork@gmail.com>
2020-01-10 15:19:27 +0300

    shittt

--- lala/readFile.js
+++ lala/readFile.js
@@ -1,8 +1,19 @@
 const fs = require(`fs`);
+const asyncRainbowText = require(`./util/async-rainbow-text`);
 
-fs.readFile(__filename, `r`, (err, data) => {
-    if(err){
-        return console.error(err.message);
-    }
-    return console.log(data);
-})
+
+// fs.readFile(__filename, `r`, (err, data) => {
+//     if(err){
+//         return console.error(err.message);
+//     }
+//     return console.log(data);
+// })
+
+let lalal = async () => {
+    console.log(`-`);
+    await asyncRainbowText(`lalla`, 1000);
+    console.log(`-`);
+    console.log(`-`);
+}
+
+lalal();
