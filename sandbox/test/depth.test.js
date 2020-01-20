3bfd46f821a144d6ae62a03f77bb93ded56be959
dnagafonov <denisagafonovwork@gmail.com>
2020-01-20 14:12:50 +0300

    fix assert comparision

--- sandbox/test/depth.test.js
+++ sandbox/test/depth.test.js
@@ -4,18 +4,18 @@ const { depth } = require(`../codewars`);
 describe('Basic tests', function() {
 
     it('returns the right depth when an object is passed in', function() {
-      assert(depth({}), 0);
-      assert(depth({a: 1}), 1);
-      assert(depth({a: 1, b: {c: 2}}), 2);
+      assert.equal(depth({}), 0);
+      assert.equal(depth({a: 1}), 1);
+      assert.equal(depth({a: 1, b: {c: 2}}), 2);
     });
 
     it('accounts for `null`', function() {
-        assert(depth(null), 0);
+        assert.equal(depth(null), 0);
     });
 
     it('ignores arrays', function() {
-        assert(depth(['a', 'b', 'c']), 0);
-        assert(depth(['a', ['b', 'c']]), 0);
+        assert.equal(depth(['a', 'b', 'c']), 0);
+        assert.equal(depth(['a', ['b', 'c']]), 0);
     });
     
   });
